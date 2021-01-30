95d9cd8ae7d1891e97bcf0cf0c643b6fa5926e3d
ibldzn <haytsam.adzka@gmail.com>
2021-01-30 20:12:52 +0700

    added missing export function

--- yakuza-0-legend-style-fix/src/proxy/proxy.cpp
+++ yakuza-0-legend-style-fix/src/proxy/proxy.cpp
@@ -4,8 +4,16 @@
 #  define PROXY_API extern "C" __declspec(dllexport)
 #endif
 
+static proxy::CreateDXGIFactory_t proxy::o_CreateDXGIFactory = nullptr;
 static proxy::CreateDXGIFactory1_t proxy::o_CreateDXGIFactory1 = nullptr;
 static proxy::CreateDXGIFactory2_t proxy::o_CreateDXGIFactory2 = nullptr;
+static proxy::DXGIDeclareAdapterRemovalSupport_t proxy::o_DXGIDeclareAdapterRemovalSupport = nullptr;
+static proxy::DXGIGetDebugInterface1_t proxy::o_DXGIGetDebugInterface1 = nullptr;
+
+PROXY_API HRESULT CreateDXGIFactory(REFIID riid, void** ppFactory)
+{
+    return proxy::o_CreateDXGIFactory(riid, ppFactory);
+}
 
 PROXY_API HRESULT CreateDXGIFactory1(REFIID riid, void** ppFactory)
 {
@@ -16,6 +24,17 @@ PROXY_API HRESULT CreateDXGIFactory2(UINT Flag, REFIID riid, void** ppFactory)
 {
     return proxy::o_CreateDXGIFactory2(Flag, riid, ppFactory);
 }
+
+PROXY_API HRESULT DXGIDeclareAdapterRemovalSupport()
+{
+    return proxy::o_DXGIDeclareAdapterRemovalSupport();
+}
+
+PROXY_API HRESULT DXGIGetDebugInterface1(UINT Flags, REFIID riid, void** pDebug)
+{
+    return proxy::o_DXGIGetDebugInterface1(Flags, riid, pDebug);
+}
+
 
 #undef PROXY_API
 
@@ -30,6 +49,10 @@ void proxy::init()
     if (!dxgi)
         throw std::runtime_error("proxy::init - Failed to load original dxgi.dll!");
 
+    proxy::o_CreateDXGIFactory = reinterpret_cast<proxy::CreateDXGIFactory_t>(GetProcAddress(dxgi, "CreateDXGIFactory"));
+    if (!proxy::o_CreateDXGIFactory)
+        throw std::runtime_error("proxy::init - Failed to get address for CreateDXGIFactory!");
+
     proxy::o_CreateDXGIFactory1 = reinterpret_cast<proxy::CreateDXGIFactory1_t>(GetProcAddress(dxgi, "CreateDXGIFactory1"));
     if (!proxy::o_CreateDXGIFactory1)
         throw std::runtime_error("proxy::init - Failed to get address for CreateDXGIFactory1!");
@@ -37,4 +60,12 @@ void proxy::init()
     proxy::o_CreateDXGIFactory2 = reinterpret_cast<proxy::CreateDXGIFactory2_t>(GetProcAddress(dxgi, "CreateDXGIFactory2"));
     if (!proxy::o_CreateDXGIFactory2)
         throw std::runtime_error("proxy::init - Failed to get address for CreateDXGIFactory2!");
+
+    proxy::o_DXGIDeclareAdapterRemovalSupport = reinterpret_cast<proxy::DXGIDeclareAdapterRemovalSupport_t>(GetProcAddress(dxgi, "DXGIDeclareAdapterRemovalSupport"));
+    if (!proxy::o_DXGIDeclareAdapterRemovalSupport)
+        throw std::runtime_error("proxy::init - Failed to get address for DXGIDeclareAdapterRemovalSupport!");
+
+    proxy::o_DXGIGetDebugInterface1 = reinterpret_cast<proxy::DXGIGetDebugInterface1_t>(GetProcAddress(dxgi, "DXGIGetDebugInterface1"));
+    if (!proxy::o_DXGIGetDebugInterface1)
+        throw std::runtime_error("proxy::init - Failed to get address for DXGIGetDebugInterface1!");
 }
--- yakuza-0-legend-style-fix/src/proxy/proxy.h
+++ yakuza-0-legend-style-fix/src/proxy/proxy.h
@@ -13,9 +13,18 @@ namespace proxy
 {
     void init();
 
+    using CreateDXGIFactory_t = HRESULT(*)(REFIID riid, void** ppFactory);
+    extern CreateDXGIFactory_t o_CreateDXGIFactory;
+
     using CreateDXGIFactory1_t = HRESULT(*)(REFIID riid, void** ppFactory);
     extern CreateDXGIFactory1_t o_CreateDXGIFactory1;
 
     using CreateDXGIFactory2_t = HRESULT(*)(UINT Flags, REFIID riid, void** ppFactory);
     extern CreateDXGIFactory2_t o_CreateDXGIFactory2;
+
+    using DXGIDeclareAdapterRemovalSupport_t = HRESULT(*)();
+    extern DXGIDeclareAdapterRemovalSupport_t o_DXGIDeclareAdapterRemovalSupport;
+
+    using DXGIGetDebugInterface1_t = HRESULT(*)(UINT Flags, REFIID riid, void** pDebug);
+    extern DXGIGetDebugInterface1_t o_DXGIGetDebugInterface1;
 }
