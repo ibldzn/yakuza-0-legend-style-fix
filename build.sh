05706a3434dc994992621b093d382b84d545053e
ibldzn <haytsam.adzka@gmail.com>
2023-01-25 20:25:09 +0700

    refactor: rewrite the build script

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-options=("Debug" "Release" "RelWithDebInfo" "MinSizeRel")
-if [[ " ${options[*]} " == *" $1 "* ]] ; then
-  compile_mode="$1"
-  rest_args="${@:2}"
-else
-  compile_mode="Debug"
-  rest_args="$@"
-fi
+script_dir="$(printf "%q " "$(realpath -s "$0")")"
+cur_dir="${script_dir%/*}"
+rest_args=()
 
-echo "Mode: $compile_mode"
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --dbg) build_type="Debug";          shift ;;
+    --rel) build_type="Release";        shift ;;
+    --msr) build_type="MinSizeRel";     shift ;;
+    --rwd) build_type="RelWithDebInfo"; shift ;;
+       -b) shift; build_folder="$1";    shift ;;
+        *) rest_args+=("$1");           shift ;;
+  esac
+done
 
-build_dir=_build
-cur_dir=$(dirname $(realpath -s $0))
+[[ -z "$build_type" ]]   && build_type="Debug"
+[[ -z "$build_folder" ]] && build_folder="_build"
 
-cmake -S $cur_dir -B $cur_dir/$build_dir -DCMAKE_BUILD_TYPE=$compile_mode $rest_args && \
-cmake --build $cur_dir/$build_dir --config $compile_mode
+cmake -S "$cur_dir" -B "$cur_dir/$build_folder" -DCMAKE_BUILD_TYPE="$build_type" "${rest_args[@]}"
+cmake --build "$cur_dir/$build_folder" --config "$build_type" --parallel
